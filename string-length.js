9f8952bb4ebffe83f60e3892f95fc4af5524d68f
ielole <ieloletin@gmail.com>
2016-10-23 14:05:50 -0400

    Work on strings and string-length.
    Added files strings.js and string-length.

---
+++ string-length.js
@@ -0,0 +1,7 @@
+/* 'use strict';
+
+let example = 'example string';
+console.log(example.length); */
+
+var example = 'example string';
+console.log(example.length);
---
+++ strings.js
@@ -0,0 +1,7 @@
+/*'use strict';
+
+let someString = "Why won't strict mode work?";
+console.log(someString)*/
+
+var someString ='this is a string';
+console.log(someString);
