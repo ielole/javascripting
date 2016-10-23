a0d004fd824d59a107498e5cb00419388cf02be2
ielole <ieloletin@gmail.com>
2016-10-23 13:51:08 -0400

    Edit of variables.
    Commented out proper code and wrote additional code to pass test.

--- variables.js
+++ variables.js
@@ -1,4 +1,9 @@
-'use strict';
+/*'use strict';
 
 let example = 'some string';
+console.log(example);*/
+
+//proper code above; to pass test see below
+
+var example = 'some string';
 console.log(example);
