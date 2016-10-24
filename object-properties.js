bbda34fae33e40867d40d0e0296a2dde993e87f9
ielole <ieloletin@gmail.com>
2016-10-24 08:39:01 -0400

    Work with functions, objects and scope.
    Practiced creating objects and function. Learned about
    global vs local scope.

---
+++ object-properties.js
@@ -0,0 +1,12 @@
+/* 'use strict';
+
+let food = {
+types: 'only pizza'
+};
+console.log(food.types);
+*/
+
+var food = {
+types: 'only pizza'
+};
+console.log(food.types);
---
+++ object.js
@@ -0,0 +1,17 @@
+/* 'use strict';
+
+let pizza = {
+  toppings: ['cheese','sauce','pepperoni'],
+  crust: 'deep dish',
+  serves: 2
+};
+console.log(pizza);
+*/
+
+
+var pizza = {
+  toppings: ['cheese','sauce','pepperoni'],
+  crust: 'deep dish',
+  serves: 2
+};
+console.log(pizza);
