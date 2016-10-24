083a7461f4de3a48887ace8c8011d65e1c00062d
ielole <ieloletin@gmail.com>
2016-10-24 07:01:40 -0400

    Work with arrays.
    Learned about filtering arrays and practiced the others.

---
+++ accessing-array-values.js
@@ -0,0 +1,9 @@
+/* 'use strict'
+
+let food = ['apple','pizza','pear'];
+console.log(food[1]);
+
+*/
+
+var food = ['apple','pizza','pear'];
+console.log(food[1]);
---
+++ arrays.js
@@ -0,0 +1,8 @@
+/* 'use strict';
+
+let pizzaToppings = ['tomato sauce','cheese',pepperoni];
+  console.log(pizzaToppings);
+*/
+
+var pizzaToppings = ['tomato sauce','cheese','pepperoni'];
+  console.log(pizzaToppings);
